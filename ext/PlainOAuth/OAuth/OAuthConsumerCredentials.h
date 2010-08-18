ca171c840f11b0b21cd69583ba92f1cc12b658a6
Greg Pascale <greg.pascale@gmail.com>
2010-08-18 01:24:43 -0700

    Adding OAuthConsumerCredentials.h to repo. This is ok since I moved it out of the OAuth lib so it is only compiled in the demo app.

---
+++ ext/PlainOAuth/OAuth/OAuthConsumerCredentials.h
@@ -0,0 +1,10 @@
+/*
+ *  OAuthConsumerCredentials.h
+ *
+ *  Created by Jaanus Kase on 16.01.10.
+ *  Copyright 2010. All rights reserved.
+ *
+ */
+
+#define OAUTH_CONSUMER_KEY @"JIKslP4pBl1KL6BgVtLyA"
+#define OAUTH_CONSUMER_SECRET @"YCuVKlODpYYMcgFf4EJoV9JhBVSO3k8w9sgppXCqE"
